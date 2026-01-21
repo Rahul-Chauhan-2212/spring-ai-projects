93e2f242821371df3e4982db512975dbab27275d
Rahul Chauhan <rchauhan9102@gmail.com>
2026-01-21 23:35:50 +0530

    Add module for Open AI Communication

---
+++ openai-communication/src/main/java/com/spring/ai/openai/communication/OpenAICommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ai.openai.communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAICommunicationApplication {
+
+	public static void main(String[] args) {
+    SpringApplication.run(OpenAICommunicationApplication.class, args);
+	}
+
+}
