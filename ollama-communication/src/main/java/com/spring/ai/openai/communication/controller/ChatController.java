dd67f11775406eee3b5a8070c361b3c0490004a4
Rahul Chauhan <rchauhan9102@gmail.com>
2026-01-22 00:00:09 +0530

    Ollama communication

---
+++ ollama-communication/src/main/java/com/spring/ai/openai/communication/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.spring.ai.openai.communication.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ChatController {
+
+  private ChatClient chatClient;
+
+  public ChatController(ChatClient.Builder builder) {
+    chatClient = builder.build();
+
+  }
+
+  @GetMapping("/chat")
+  public ResponseEntity<String> chat(@RequestParam(value = "q", required = true) String q) {
+    var resultResult = chatClient.prompt(q).call().content();
+    return ResponseEntity.ok(resultResult);
+
+  }
+
+}
---
+++ ollama-communication/src/test/java/com/spring/ai/openai/communication/OllamaCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai.openai.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
